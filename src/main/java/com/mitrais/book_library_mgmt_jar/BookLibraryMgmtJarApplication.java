4c3dfc11846b6ea285e3276eb1a3ab060ca65c9e
aji_mulyadi <adjie.mulyadi7x@gmail.com>
2019-02-07 07:32:52 +0700

    add all

---
+++ src/main/java/com/mitrais/book_library_mgmt_jar/BookLibraryMgmtJarApplication.java
@@ -0,0 +1,14 @@
+package com.mitrais.book_library_mgmt_jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryMgmtJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLibraryMgmtJarApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/mitrais/book_library_mgmt_jar/BookLibraryMgmtJarApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mitrais.book_library_mgmt_jar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookLibraryMgmtJarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
